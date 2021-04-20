14a9fb720f2106d953b3b7dd6de5a19a2bab51d7
Bruno Arruda <bruno.arruda-impulso@m4u.com.br>
2021-04-20 10:35:57 -0300

    adding required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users, only: :create
+  resources :invoices, param: :invoice_id
+  resources :chargebacks, only: :create
+  put '/invoices/:invoice_id/pay', to: 'transactions#pay'
+  put '/invoices/:invoice_id/invalidate', to: 'transactions#invalidate'
+
+  post '/auth/login', to: 'authentication#login'
+  get '/*a', to: 'application#not_found'
 end
